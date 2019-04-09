1c8e792d9192fdbb3ff3464a98175a14e4bf8240
divisor <div@altertech.com>
2019-04-09 04:08:09 +0200

    curl s

--- sensor-set.sh
+++ sensor-set.sh
@@ -13,7 +13,7 @@ JQ=$?
 R="{ \"jsonrpc\": \"2.0\", \"id\": \"`date '+%N'`\", \"method\": \"update\", "
 R="$R\"params\": { \"k\": \"${MASTERKEY}\", \"i\": \"greenhouse${1}/env/${2}\", \"s\": 1, \"v\": \"${3}\" } }"
 
-echo $R | curl -v -m5 --header "Content-Type: application/json" --data @- \
+echo $R | curl -sv -m5 --header "Content-Type: application/json" --data @- \
   http://10.27.11.10${1}:8812/jrpc | ( [ ${JQ} -eq 0 ] && jq || cat )
 
 echo
