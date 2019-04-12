0110bb6e62d5ce0bba6d71ecc8d21f389d61bc5f
divisor <div@altertech.com>
2019-04-12 19:44:09 +0200

    restart

--- .online/_online.sh
+++ .online/_online.sh
@@ -12,6 +12,7 @@ DEFAULTKEY=`head -1024 /dev/urandom | sha256sum | awk '{ print $1 }'`
 
 sed "s/- MASTERKEY=.*/- MASTERKEY=${MASTERKEY}/g" docker-compose.yml | \
   sed "s/- DEFAULTKEY=.*/- DEFAULTKEY=${DEFAULTKEY}/g" | \
+  sed 's/hostname:/restart: always\n    hostname:/g' | \
   grep -v $'ports:\n.*\- "8828:8828"' > docker-compose-online-demo.yml
 
 sh ./.online/_online-deploy.sh || exit 1
