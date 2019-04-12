02a835f9b333b640a195c77bca438983bb72a6a2
divisor <div@altertech.com>
2019-04-12 19:51:08 +0200

    crontab str

--- .online/_online.sh
+++ .online/_online.sh
@@ -10,6 +10,9 @@ cd ..
 MASTERKEY=`head -1024 /dev/urandom | sha256sum | awk '{ print $1 }'`
 DEFAULTKEY=`head -1024 /dev/urandom | sha256sum | awk '{ print $1 }'`
 
+D=`dirname $0`
+D=`realpath $D`
+
 sed "s/- MASTERKEY=.*/- MASTERKEY=${MASTERKEY}/g" docker-compose.yml | \
   sed "s/- DEFAULTKEY=.*/- DEFAULTKEY=${DEFAULTKEY}/g" | \
   sed 's/hostname:/restart: always\n    hostname:/g' | \
@@ -19,3 +22,8 @@ sh ./.online/_online-deploy.sh || exit 1
 python3 ./.online/_online-demo-initial-generator.py || exit 1
 echo "--------------------------------------------------------"
 echo "Online demo deployement completed"
+echo ""
+echo "crontab string"
+echo ""
+echo "* * * * *    root    $D/simulate_se.py -k ${MASTERKEY}"
+echo ""
