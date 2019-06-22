73108311b5dc3e7848021f52e92a0b1121ff0982
divisor <div@altertech.com>
2019-06-23 01:13:35 +0200

    fixes online demo

--- .online/_online.sh
+++ .online/_online.sh
@@ -19,7 +19,9 @@ touch docker-compose-online-demo.yml
 chmod 600 docker-compose-online-demo.yml
 
 sed "s/- MASTERKEY=.*/- MASTERKEY=${MASTERKEY}/g" docker-compose.yml | \
-  sed "s/- DEFAULTKEY=.*/- DEFAULTKEY=${DEFAULTKEY}/g" > docker-compose-online-demo.yml
+  sed "s/- DEFAULTKEY=.*/- DEFAULTKEY=${DEFAULTKEY}/g" | \
+  sed 's/image:/restart: always\n    image:/g' | \
+  grep -v $'ports:\n.*\- "8828:8828"' > docker-compose-online-demo.yml
 
 sh ./.online/_online-deploy.sh || exit 1
 python3 ./.online/_online-demo-initial-generator.py || exit 1
